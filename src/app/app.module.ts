9f38f3c2d0c25a2ccb4ea9875139e58581e081a8
Sergi Philipsen <philipsen.sergi@gmail.com>
2018-12-06 16:09:43 +0100

    Add routing to App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'start',
+    component: HeaderComponent
+  },
+  { path: '',
+    redirectTo: '/start',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HeaderComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,11 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
