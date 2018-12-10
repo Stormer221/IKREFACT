918cd56bc729d9a2ff924ad628766f416531bdbc
Tom de Jong <dejongtom01@gmail.com>
2018-12-10 12:24:31 +0100

    components made

---
+++ src/app/contacts/contact-model.ts
@@ -0,0 +1,11 @@
+import {OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+export class Contact implements OnInit {
+
+  constructor(private name: string, ) {
+  }
+
+  ngOnInit() {
+  }
+}
