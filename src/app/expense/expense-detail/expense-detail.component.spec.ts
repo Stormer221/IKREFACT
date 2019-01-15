29003211eba3dbe817059325d28be50d0d64e3c5
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-01-15 17:28:36 +0100

    Add expense detail

--- src/app/expense/expense-detail/expense-detail.component.spec.ts
+++ src/app/expense/expense-detail/expense-detail.component.spec.ts
@@ -1,20 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {ExpenseItemComponent} from './expense-item.component';
+import {ExpenseDetailComponent} from './expense-detail.component';
 
-describe('ExpenseItemComponent', () => {
-  let component: ExpenseItemComponent;
-  let fixture: ComponentFixture<ExpenseItemComponent>;
+describe('ExpenseDetailComponent', () => {
+  let component: ExpenseDetailComponent;
+  let fixture: ComponentFixture<ExpenseDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ExpenseItemComponent]
+      declarations: [ExpenseDetailComponent]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ExpenseItemComponent);
+    fixture = TestBed.createComponent(ExpenseDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
