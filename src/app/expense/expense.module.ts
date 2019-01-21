59b53578a6274ec522f34236f06679b595b283d8
Sergi Philipsen <philipsen.sergi@gmail.com>
2019-01-21 11:04:01 +0100

    Put Expense stuff in its own module

--- src/app/expense/expense.module.ts
+++ src/app/expense/expense.module.ts
@@ -6,12 +6,21 @@ import {ExpenseEditComponent} from './expense-edit/expense-edit.component';
 import {ExpenseDetailComponent} from './expense-detail/expense-detail.component';
 import {SharedModule} from '../shared/shared.module';
 import {ExpenseService} from './expense.service';
+import {AppRoutingModule} from "../app-routing.module";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
+  declarations: [
+    ExpenseComponent,
+    ExpenseListComponent,
+    ExpenseEditComponent,
+    ExpenseDetailComponent
+  ],
   imports: [
     CommonModule,
-    SharedModule,
-
+    AppRoutingModule,
+    FormsModule,
+    SharedModule
   ],
   providers: [
     ExpenseService
