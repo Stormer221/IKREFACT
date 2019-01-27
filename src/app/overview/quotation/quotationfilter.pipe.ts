5a598bc05fbc5377932f1df1196526ec17f16267
Mark Bueno <markbuenoguzman@hotmail.com>
2019-01-27 20:40:59 +0100

    quotation/quotation-list/quotation-list quotation/quotationfilter searchbar works!!

---
+++ src/app/overview/quotation/quotationfilter.pipe.ts
@@ -0,0 +1,23 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+
+@Pipe({
+  name: 'offertefilter'
+})
+export class QuotationFilterPipe implements PipeTransform {
+
+
+  transform(item: any, searchTerm: string): any {
+    if (!item || !searchTerm) {
+      return item;
+    }
+    return item.filter(it => {
+      const title = it.title.toString().includes(searchTerm);
+      const description = it.description.toLowerCase().includes(searchTerm.toLowerCase());
+      const date = it.date.toLowerCase().includes(searchTerm.toLowerCase());
+      console.log(title + description + date);
+      return (title + description + date);
+    });
+  }
+
+}
